e61bb24d967eb9ab30c3969be388d29cfa513023
VReagle <96657545+VReagle@users.noreply.github.com>
2026-05-21 12:45:01 +0530

    Add Var.c to display student information

---
+++ Var.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  char name[] ="Amal";
+  int Age = 22;
+  float GPA = 3.75;
+  char Grade = 'A';
+    printf("Student Name: %s\n", name);
+    printf("Student Age: %d\n", Age);
+    printf("Student GPA: %.2f\n", GPA);
+    printf("Student Grade: %c\n", Grade);
+
+return 0;
+}
