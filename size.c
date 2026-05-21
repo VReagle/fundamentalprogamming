9d89c8a827cc618698c21cab29b0cbceccb9ee87
VReagle <96657545+VReagle@users.noreply.github.com>
2026-05-21 12:46:26 +0530

    Display sizes of int, float, double, and char
    Added print statements to display sizes of various data types.

---
+++ size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  
+    printf("int size: %zu bytes\n", sizeof(int));
+    printf("float size: %zu bytes\n", sizeof(float));
+    printf("double size: %zu bytes\n", sizeof(double));
+    printf("char size: %zu bytes\n", sizeof(char));
+  
+return 0;
+}
