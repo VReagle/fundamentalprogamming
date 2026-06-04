eca3b1380536d4be3446f89966751ba8b2e5f246
VReagle <vreagle8@gmail.com>
2026-06-04 09:48:40 +0530

    operator

---
+++ Operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a = 15;
+    int b = 5;
+    int c = 0;
+
+    printf("addition of a and b: %d\n", a+ b);
+    printf("subtraction of a and b: %d\n", a - b);
+    printf("multiplication of a and b: %d\n", a * b);
+    printf("division of a and b: %d\n", a / b);
+    printf("remainder of a and b: %d\n", a % b);
+
+}
