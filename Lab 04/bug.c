27fd854751ca0777c7f40a7875592db2b4f6a4d6
VReagle <vreagle8@gmail.com>
2026-06-11 09:11:47 +0530

    bug

--- Lab 04/bug.c
+++ Lab 04/bug.c
@@ -2,7 +2,7 @@
 int main(){
 
 int x = 5;
-    if (x = 0)                     /* Bug 1 */ 
+    if (x = 0)                     /* Bug 1 */  //if (x == 0)
         printf("Zero\n");
     else 
         printf("Non-zero\n");
@@ -11,7 +11,7 @@ int x = 5;
         case 5: 
             printf("Five\n"); 
         case 6:                    /* Bug 2: what happens here? */ 
-            printf("Six\n"); 
+            printf("Six\n");
     }
 
 }
