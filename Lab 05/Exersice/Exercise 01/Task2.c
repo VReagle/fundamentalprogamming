dd7eaf65223d91053c01e5897396a82410262d9c
VReagle <vreagle8@gmail.com>
2026-06-25 09:58:18 +0530

    Fix

--- Lab 05/Exersice/Exercise 01/Task2.c
+++ Lab 05/Exersice/Exercise 01/Task2.c
@@ -2,18 +2,23 @@
 
 // Fibonacci Number
 
-int fibonacci(){
-    
-
-
+int fibonacci(int n){
+    if (n <= 0) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+
 int main(){
+    int num;
 
+    printf("Enter Number : ");
+    scanf("%d", &num);
 
-
-
-
-
-
+    printf("%d place fibonacci Number : %d\n", num, fibonacci(num));
+    return 0;
 }
