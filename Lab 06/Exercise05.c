4e05334a24efcc0685f90383dfa62aca29d63843
VReagle <vreagle8@gmail.com>
2026-07-09 13:30:43 +0530

    fix

--- Lab 06/Exercise05.c
+++ Lab 06/Exercise05.c
@@ -30,9 +30,17 @@ int main(){
     {
         for (col = 0; col < 2; col++)
         {
-            printf("%4d",C[raw][col]);
+            C[raw][col] = A[raw][col] + B[raw][col];
+        }
+    }
+
+    for (raw = 0; raw < 3; raw++)
+    {
+        for (col = 0; col < 2; col++)
+        {
+            printf("%4d", C[raw][col]);
         }
         printf("\n");
     }
-    
+    return 0;
 }
